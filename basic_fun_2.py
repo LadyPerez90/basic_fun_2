0742f1d463c073e0cc237aee998c5059167bf168
LadyPerez90 <josefa.groce@gmail.com>
2021-04-11 19:05:26 -0500

    first commit

---
+++ basic_fun_2.py
@@ -0,0 +1,39 @@
+# 01 Countdown
+# def countdown():
+#     for x in range (5,-1,-1):
+#         print(x)
+# countdown()
+
+# 02 Print and Return
+# def a(arr):
+#     print(arr[0])
+#     return arr[1]
+# a([1,2])
+
+# 03 First Plug Length
+# def plug_len(arr = ([1,2,3,4,5])):
+#     return arr[0] + len(arr)
+# print(plug_len())
+
+
+
+# 04 Values Greater Than Second
+# def greater(arr):
+#     new_arr = []
+#     x = arr[1]
+#     for y in range(len(arr)):
+#         if arr[y] > x:
+#             new_arr.append(arr[y])
+#     print(len(new_arr))
+#     return new_arr
+
+# greater([5,2,3,2,1,4])
+
+# 05 This Length, This Value
+# def len_val(size, value):
+#     x = []
+#     for y in range(size):
+#         x.append(value)
+#     print(x)
+
+# len_val (4,7)
